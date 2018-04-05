0b57128d01f99436f66f5e58d78649cdb20dca77
Isabel Orticelle <iorticel@udel.edu>
2018-04-05 10:41:05 -0400

    js practice

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,11 @@
+let blist=["job", "graduate", "travel"];
+console.log(blist.length);
+blist.push("drink wine")
+console.log(blist.length);
+
+
+
+//for loop
+for(let i=0;i<blist.length;i++){
+console.log(blist[i])
+}
