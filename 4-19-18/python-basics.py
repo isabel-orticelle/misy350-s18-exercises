e5794d6248c638768bc9f45ea6064e7973440086
Isabel Orticelle <iorticel@udel.edu>
2018-04-19 10:23:45 -0400

    python exercise

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first pyton code
+greeting="hello"
+name="Isabel"
+print greeting #this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting +" " +name+"!"
+print "%s %d! How are you doing today?"%(greeting, 5728)
