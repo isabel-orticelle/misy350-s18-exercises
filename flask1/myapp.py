d9d62e966ef1560728c5cb928cf326cf4c54acff
Isabel Orticelle <iorticel@udel.edu>
2018-05-07 16:03:23 -0400

    misy350s18-myapp

--- flask1/myapp.py
+++ flask1/myapp.py
@@ -1,18 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "hello Isabel Orticelle"
+    # return "hello friend"
+    return render_template('index.html')
 
 
-@app.route('/hello')
-def user():
-    import random
+@app.route('/users/<string:username>')
+def users(username):
+    #return "<h1>hello %s</h1>" % username
+    return render_template('users.html', uname=username)
 
-    greeting_list = ['Ciao', 'Hei', 'Salut', 'Hola', 'Nihao']
-    return random.choice(greeting_list)
+
+
+@app.route('/form')
+def form_basics():
+        # return "hello friend"
+    return render_template('form.html')
+
+@app.route('/submit-form')
+def form_submit():
+        # return "hello friend"
+    return render_template('submit-form.html')
 
 
 if __name__ == '__main__':
